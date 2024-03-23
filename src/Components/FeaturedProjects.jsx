6ef4a7f83a0831bae2f1bf1288eb95546df6d7f7
husnainalyy <alyhusnaiin@gmail.com>
2024-03-23 15:10:25 +0500

    class to classNam

--- src/Components/FeaturedProjects.jsx
+++ src/Components/FeaturedProjects.jsx
@@ -41,7 +41,7 @@ const FeaturedProjects = () => {
       </div>
       <button
         type="button"
-        class="inline-block rounded-full bg-neutral-800 px-12 py-3 mt-8 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-dark-3 transition duration-150 ease-in-out hover:bg-neutral-700 hover:shadow-dark-2 focus:bg-neutral-700 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-dark-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+        className="inline-block rounded-full bg-neutral-800 px-12 py-3 mt-8 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-dark-3 transition duration-150 ease-in-out hover:bg-neutral-700 hover:shadow-dark-2 focus:bg-neutral-700 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-dark-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
       >
         MORE PROJECTS
       </button>
--- src/Components/Purchase.jsx
+++ src/Components/Purchase.jsx
@@ -25,9 +25,9 @@ const Purchase = () => {
           Income.it has developed a passionate audience of more than 25 million
           active monthly users.
         </p>
-        <button
+        <button 
           type="button"
-          class="absolute z-3 bottom-7 lg:bottom-16 inline-block rounded-full bg-neutral-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-600 shadow-light-3 transition duration-150 ease-in-out hover:bg-neutral-200 hover:shadow-light-2 focus:bg-neutral-200 focus:shadow-light-2 focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-light-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+          className="absolute z-3 bottom-7 lg:bottom-16 inline-block rounded-full bg-neutral-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-600 shadow-light-3 transition duration-150 ease-in-out hover:bg-neutral-200 hover:shadow-light-2 focus:bg-neutral-200 focus:shadow-light-2 focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-light-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
         >
           PURCHASE NOW
         </button>
